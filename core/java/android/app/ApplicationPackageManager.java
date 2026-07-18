bda44484c7215245144db7ae1172552bf083ad4c
saitama-21 <69281812+saitama-21@users.noreply.github.com>
2026-07-18 10:36:32 +0000

    Revert "base: core: Blacklist Pixel 2017 and 2018 exclusive for Google Photos"
    This reverts commit b8189845835ff594a735f7a7cfedcc9fdbf40cf1.

--- core/java/android/app/ApplicationPackageManager.java
+++ core/java/android/app/ApplicationPackageManager.java
@@ -819,9 +819,7 @@ public class ApplicationPackageManager extends PackageManager {
                 name.contains("PIXEL_2020_MIDYEAR_EXPERIENCE") ||
                 name.contains("PIXEL_2019_EXPERIENCE") ||
                 name.contains("PIXEL_2019_PRELOAD") ||
-                name.contains("PIXEL_2019_MIDYEAR_EXPERIENCE") ||
-                name.contains("PIXEL_2018_PRELOAD") ||
-                name.contains("PIXEL_2017_PRELOAD")) {
+                name.contains("PIXEL_2019_MIDYEAR_EXPERIENCE")) {
             return false;
         }
         return mHasSystemFeatureCache.query(new HasSystemFeatureQuery(name, version));
