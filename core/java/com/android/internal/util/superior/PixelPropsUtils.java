fff2eab84ea8d1b22bda7ed139604631cdc4fec7
saitama-21 <69281812+saitama-21@users.noreply.github.com>
2026-07-18 10:35:31 +0000

    Revert "PixelpropUtils: Update fingerprints to January 2023 Release"
    This reverts commit f80118ab786fa1afb978249d49de397642a35964.

--- core/java/com/android/internal/util/superior/PixelPropsUtils.java
+++ core/java/com/android/internal/util/superior/PixelPropsUtils.java
@@ -148,6 +148,7 @@ public class PixelPropsUtils {
         propsToChangePixel7Pro.put("PRODUCT", "cheetah");
         propsToChangePixel7Pro.put("MODEL", "Pixel 7 Pro");
 <<<<<<< HEAD
+<<<<<<< HEAD
         propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230205.002/9471150:user/release-keys");
         propsToChangePixel6Pro = new HashMap<>();
         propsToChangePixel6Pro.put("BRAND", "google");
@@ -159,13 +160,20 @@ public class PixelPropsUtils {
 =======
         propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.230105.001.A2/9325679:user/release-keys");
 >>>>>>> parent of eee768a43c2c (PixelPropsUtils: Add toggle for Gaming and google photos spoofs)
+=======
+        propsToChangePixel7Pro.put("FINGERPRINT", "google/cheetah/cheetah:13/TQ1A.221205.011/9244662:user/release-keys");
+>>>>>>> parent of f80118ab786f (PixelpropUtils: Update fingerprints to January 2023 Release)
         propsToChangePixel5 = new HashMap<>();
         propsToChangePixel5.put("BRAND", "google");
         propsToChangePixel5.put("MANUFACTURER", "Google");
         propsToChangePixel5.put("DEVICE", "redfin");
         propsToChangePixel5.put("PRODUCT", "redfin");
         propsToChangePixel5.put("MODEL", "Pixel 5");
+<<<<<<< HEAD
         propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:13/TQ1A.230205.002/9471150:user/release-keys");
+=======
+        propsToChangePixel5.put("FINGERPRINT", "google/redfin/redfin:13/TQ1A.221205.011/9244662:user/release-keys");
+>>>>>>> parent of f80118ab786f (PixelpropUtils: Update fingerprints to January 2023 Release)
         propsToChangePixelXL = new HashMap<>();
         propsToChangePixelXL.put("BRAND", "google");
         propsToChangePixelXL.put("MANUFACTURER", "Google");
